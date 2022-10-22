e935f5f87acab35b14bef314cde6e8f35660e6c1
jarricd <damianherman@protonmail.com>
2022-10-22 20:22:57 +0200

    feat: The rest of network archis

--- MLP/arch.py
+++ MLP/arch.py
@@ -51,13 +51,67 @@ class MLP10(torch.nn.Module):
             # torch.nn.Flatten(), # might be needed to pool everything into one layer
             # apply following function: y = x * A^T + b, where y is out, A - weights, b is bias
             # if an input is [60000, 2], then first param is num features, output is num features we get
-            torch.nn.Linear(10, 8),
-            torch.nn.ReLU(),
-            torch.nn.Linear(8, 6),
-            torch.nn.ReLU(),
-            torch.nn.Linear(6, 8),
+            torch.nn.Linear(10, 6),
             torch.nn.ReLU(),
             torch.nn.Linear(6, 10),
+            torch.nn.Softmax()
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+class MLP20(torch.nn.Module):
+    """
+    REMEMBER:
+    For matrix multiplication,
+    the number of columns in the first matrix must
+    be equal to the number of rows in the second matrix.
+    The result matrix has the number of
+    rows of the first and the number of columns of the second matrix.
+    """
+    def __init__(self):
+        super().__init__()
+        # this code is written for LDA-reduced dataset to 2 dimensions
+        # the model it's hopeless
+        # since we have input 10 features, we have to 'transplant' it into more output features
+        # we need more dimensions and modified architectures
+        self.layers = torch.nn.Sequential(
+            # torch.nn.Flatten(), # might be needed to pool everything into one layer
+            # apply following function: y = x * A^T + b, where y is out, A - weights, b is bias
+            # if an input is [60000, 2], then first param is num features, output is num features we get
+            torch.nn.Linear(20, 15),
+            torch.nn.ReLU(),
+            torch.nn.Linear(15, 10),
+            torch.nn.Softmax()
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+
+class MLP30(torch.nn.Module):
+    """
+    REMEMBER:
+    For matrix multiplication,
+    the number of columns in the first matrix must
+    be equal to the number of rows in the second matrix.
+    The result matrix has the number of
+    rows of the first and the number of columns of the second matrix.
+    """
+    def __init__(self):
+        super().__init__()
+        # this code is written for LDA-reduced dataset to 2 dimensions
+        # the model it's hopeless
+        # since we have input 10 features, we have to 'transplant' it into more output features
+        # we need more dimensions and modified architectures
+        self.layers = torch.nn.Sequential(
+            # torch.nn.Flatten(), # might be needed to pool everything into one layer
+            # apply following function: y = x * A^T + b, where y is out, A - weights, b is bias
+            # if an input is [60000, 2], then first param is num features, output is num features we get
+            torch.nn.Linear(30, 15),
+            torch.nn.Softmax(),
+            torch.nn.Linear(15, 10),
+
         )
 
     def forward(self, x):
--- MLP/main.py
+++ MLP/main.py
@@ -2,13 +2,14 @@ import utils
 import arch
 import torch
 import pickle
-
-def train_lda_network(dataset, dset_class):
+from torch.utils.tensorboard import SummaryWriter
+def train_model(dataset, dset_class, model_class):
+    writer = SummaryWriter()
     # Define the loss function and optimizer
-    mlp = arch.MLP()
+    mlp = model_class()
     loss_function = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(mlp.parameters(), lr=1e-10) #SGD was mentioned in the slides
-    epochs = 512
+    optimizer = torch.optim.SGD(mlp.parameters(), lr=1e-5) #SGD was mentioned in the slides
+    epochs = 32
     i = 0
     # split into batches
     batch_size = 16
@@ -22,6 +23,7 @@ def train_lda_network(dataset, dset_class):
             optimizer.zero_grad()  # reset gradient
             outputs = mlp(sample)
             loss = loss_function(outputs, target_class)
+            writer.add_scalar("Loss/train", loss, epoch)
             loss.backward()
             optimizer.step()
             current_loss += loss.item()
@@ -35,58 +37,17 @@ def train_lda_network(dataset, dset_class):
         correct_guesses = 0
 
         # classify with what we have now
-        for item, truth in zip(lda_dataset["test"], lda_class["test"]):
+        for item, truth in zip(dataset["test"], dset_class["test"]):
             outputs = mlp(item)
             _, classifications = torch.max(outputs, 0)
             
             if truth == classifications:
                 correct_guesses += 1
 
-
-        accuracy = correct_guesses/len(lda_dataset['test'])
+        accuracy = correct_guesses/len(dataset['test'])
         print(f"Accuracy_lda1: {accuracy*100}")
-        pass
-
-
-def train_pca_10_network(dataset, dset_class):
-    mlp = arch.MLP10()
-    loss_function = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(mlp.parameters(), lr=1e-4)  # SGD was mentioned in the slides
-    epochs = 20
-    i = 0
-    # split into batches
-    batch_size = 16
-    splitted_batches = torch.split(lda_dataset["train"], batch_size)
-    splitted_classes = torch.split(lda_class["train"], batch_size)
-    # train
-    for epoch in range(0, epochs):
-        current_loss = 0.0
-        i = 0
-        for sample, target_class in zip(splitted_batches, splitted_classes):
-            optimizer.zero_grad()  # reset gradient
-            outputs = mlp(sample)
-            loss = loss_function(outputs, target_class)
-            loss.backward()
-            optimizer.step()
-            current_loss += loss.item()
-            if i % 500 == 0:
-                print('Loss after mini-batch %5d: %.3f. Epoch: %d' %
-                      (i + 1, current_loss / 500, epoch))
-                current_loss = 0.0
-            i += 1
-
-    with torch.no_grad():
-        correct_guesses = 0
-        # classify with what we have now
-        for item, truth in zip(lda_dataset["test"], lda_class["test"]):
-            outputs = mlp(item)
-            classification = int(torch.max(outputs))  # slash floating point to have "a closer guess"
-            if truth == classification:
-                correct_guesses += 1
-
-        accuracy = correct_guesses / len(lda_dataset['test'])
-        print(f"Accuracy_pca_10: {accuracy}")
-        pass
+        writer.add_scalar("Accuracy", accuracy*100)
+    writer.close()
 
 
 if __name__ == "__main__":
@@ -94,9 +55,17 @@ if __name__ == "__main__":
     # lda_dataset, lda_class = utils.load_data(2, "LDA")
     with open('lda.dat', "rb") as f:
         lda_dataset, lda_class = pickle.load(f)
+    with open('pca_10.dat', "rb") as f:
+        pca_dataset_10, pca_class_10 = pickle.load(f)
+    with open('pca_20.dat', "rb") as f:
+        pca_dataset_20, pca_class_20 = pickle.load(f)
+    with open('pca_30.dat', "rb") as f:
+        pca_dataset_30, pca_class_30 = pickle.load(f)
     
-    #pca_dataset_10, pca_class_10 = utils.load_data(10, "PCA")
-    with torch.cuda.device(0):
-        train_lda_network(lda_dataset, lda_class)
-    #with torch.cuda.device(0):
-         #train_pca_10_network(pca_dataset_10, pca_class_10)
+    # #pca_dataset_10, pca_class_10 = utils.load_data(10, "PCA")
+    # with torch.cuda.device(0):
+    #     train_lda_network(lda_dataset, lda_class)
+    # with torch.cuda.device(0):
+    # train_model(pca_dataset_10, pca_class_10, arch.MLP10)
+    # train_model(pca_dataset_20, pca_class_20, arch.MLP20)
+    train_model(pca_dataset_30, pca_class_30, arch.MLP30)
