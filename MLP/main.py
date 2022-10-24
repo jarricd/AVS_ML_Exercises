15137130e7ead74e65a9bc8372a08dd545a3339d
jarricd <damianherman@protonmail.com>
2022-10-24 12:49:24 +0200

    feat: changed architectures in effort to have better accuracy

--- MLP/main.py
+++ MLP/main.py
@@ -1,4 +1,3 @@
-import utils
 import arch
 import torch
 import pickle
@@ -6,15 +5,16 @@ from torch.utils.tensorboard import SummaryWriter
 from sklearn.neural_network import MLPClassifier
 
 def train_model(dataset, dset_class, model_class):
+    cuda = torch.device('cuda')
     writer = SummaryWriter()
     # Define the loss function and optimizer
-    mlp = model_class()
-    loss_function = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(mlp.parameters(), lr=1e-5) #SGD was mentioned in the slides
-    epochs = 32
+    mlp = model_class().to(cuda)
+    loss_function = torch.nn.CrossEntropyLoss().cuda()
+    optimizer = torch.optim.SGD(mlp.parameters(), lr=1e-4)
+    epochs = 45
     i = 0
     # split into batches
-    batch_size = 16
+    batch_size = 128
     splitted_batches = torch.split(dataset["train"], batch_size)
     splitted_classes = torch.split(dset_class["train"], batch_size)
     # train
@@ -23,23 +23,43 @@ def train_model(dataset, dset_class, model_class):
         i = 0
         for sample, target_class in zip(splitted_batches, splitted_classes):
             optimizer.zero_grad()  # reset gradient
-            outputs = mlp(sample)
-            loss = loss_function(outputs, target_class)
+            cuda_sample = sample.to(cuda)
+            target_class_cuda = target_class.to(cuda)
+            outputs = mlp(cuda_sample)
+            loss = loss_function(outputs, target_class_cuda)
             writer.add_scalar("Loss/train", loss, epoch)
             loss.backward()
             optimizer.step()
             current_loss += loss.item()
+
             if i % 256 == 255:
                 print('Loss after mini-batch %5d: %.3f. Epoch: %d' %
                       (i + 1, current_loss / 256, epoch + 1))
                 current_loss = 0.0
             i += 1
 
+        if epoch % 10 == 0:  # test every 10 epochs
+            correct_guesses = 0
+
+            # classify with what we have now
+            for item, truth in zip(dataset["test"], dset_class["test"]):
+                item = item.to(cuda)
+                truth = truth.to(cuda)
+                outputs = mlp(item)
+                _, classifications = torch.max(outputs, 0)
+
+                if truth == classifications:
+                    correct_guesses += 1
+            accuracy = correct_guesses / len(dataset['test'])
+            print(f"Accuracy_{epoch}: {accuracy * 100}")
+
     with torch.no_grad():
         correct_guesses = 0
 
         # classify with what we have now
         for item, truth in zip(dataset["test"], dset_class["test"]):
+            item = item.to(cuda)
+            truth = truth.to(cuda)
             outputs = mlp(item)
             _, classifications = torch.max(outputs, 0)
             
@@ -53,7 +73,7 @@ def train_model(dataset, dset_class, model_class):
 
 
 def sklearn_classifier(dataset, dataset_classes):
-    mlp_classifier = MLPClassifier(random_state=69, max_iter=200, solver="sgd", hidden_layer_sizes=(3, 500))
+    mlp_classifier = MLPClassifier(random_state=69, max_iter=300, solver="sgd", hidden_layer_sizes=(4, 200))
     mlp_classifier.fit(dataset["train"], dataset_classes["train"])
     correct_classifications = 0
     output = mlp_classifier.predict(dataset["test"])
@@ -67,28 +87,23 @@ def sklearn_classifier(dataset, dataset_classes):
 
 if __name__ == "__main__":
     torch.manual_seed(0)
-    with open('lda.dat', "rb") as f:
-        lda_dataset_nn, lda_class_nn = pickle.load(f)
-    with open('pca_10.dat', "rb") as f:
-        pca_dataset_10_nn, pca_class_10_nn = pickle.load(f)
-    with open('pca_20.dat', "rb") as f:
-        pca_dataset_20_nn, pca_class_20_nn = pickle.load(f)
-    with open('pca_30.dat', "rb") as f:
-        pca_dataset_30_nn, pca_class_30_nn = pickle.load(f)
+    #with open('lda.dat', "rb") as f:
+     #   lda_dataset_nn, lda_class_nn = pickle.load(f)
+    # with open('pca_10.dat', "rb") as f:
+    #     pca_dataset_10_nn, pca_class_10_nn = pickle.load(f)
+    # with open('pca_20.dat', "rb") as f:
+    #    pca_dataset_20_nn, pca_class_20_nn = pickle.load(f)
+    # with open('pca_30.dat', "rb") as f:
+    #    pca_dataset_30_nn, pca_class_30_nn = pickle.load(f)
 
-    with torch.cuda.device(0):
-        train_model(lda_dataset_nn, lda_class_nn, arch.MLP)
-    with torch.cuda.device(0):
-        train_model(pca_dataset_10_nn, pca_class_10_nn, arch.MLP10)
-    with torch.cuda.device(0):
-        train_model(pca_dataset_20_nn, pca_class_20_nn, arch.MLP20)
-    with torch.cuda.device(0):
-        train_model(pca_dataset_30_nn, pca_class_30_nn, arch.MLP30)
-
-    train_model(lda_dataset_nn, lda_class_nn, arch.MLP)
-    train_model(pca_dataset_10_nn, pca_class_10_nn, arch.MLP10)
-    train_model(pca_dataset_20_nn, pca_class_20_nn, arch.MLP20)
-    train_model(pca_dataset_30_nn, pca_class_30_nn, arch.MLP30)
+    #with torch.cuda.device(0):
+     #   train_model(lda_dataset_nn, lda_class_nn, arch.MLP)
+    # with torch.cuda.device(0):
+    #     train_model(pca_dataset_10_nn, pca_class_10_nn, arch.MLP10)
+    # with torch.cuda.device(0):
+    #     train_model(pca_dataset_20_nn, pca_class_20_nn, arch.MLP20)
+    # with torch.cuda.device(0):
+    #     train_model(pca_dataset_30_nn, pca_class_30_nn, arch.MLP30)
 
     with open('lda_numpy.dat', "rb") as f:
         lda_dataset, lda_class = pickle.load(f)
@@ -97,7 +112,7 @@ if __name__ == "__main__":
     with open('pca_20_numpy.dat', "rb") as f:
         pca_dataset_20, pca_class_20 = pickle.load(f)
     with open('pca_30_numpy.dat', "rb") as f:
-        pca_dataset_30, pca_class_30 = pickle.load(f)\
+        pca_dataset_30, pca_class_30 = pickle.load(f)
 
     print("LDA")
     sklearn_classifier(lda_dataset, lda_class)
